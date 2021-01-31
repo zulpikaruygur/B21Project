071081d0ed230a45c669c00a02340c5a8e4ba69f
zulpikaruygur <zulfikaruygur@gmail.com>
2021-01-31 14:21:17 +0300

    print number added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("first messages added to this file");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
     }
 }
